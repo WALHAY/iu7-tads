301fc3dac233587395df8fe9aaf1a3b90ffabe83
WALHAY <walhay@h135.net55.bmstu.ru>
2024-10-01 14:53:53 +0300

    Better entry generation

--- lab_02/src/entry_generator.c
+++ lab_02/src/entry_generator.c
@@ -1,12 +1,16 @@
 #include "../inc/entry_generator.h"
 
-char *authors[] = {"Glukhovkiy", "Asimov", "King", "Dostoevskiy", "Pushkin", "Savelyev", "Perelman", "Arbuzov"};
-char *titles1[] = {"Cool", "Awesome", "Bad", "Dummy", "Moderate"};
-char *titles2[] = {"Animals", " Nature",  " Friends", " Vibes", " Cars", " Automoblies",
-                   " People", " Student", " Apples",  " Cats",  " Dogs"};
-char *titles3[] = {" of Kamchatka", " in Nigeria", " in Africa", " of USA", " in USA", " of China", " in University"};
-char *industry[] = {"Physics", "Math", "Programming", "Law", "Chemical"};
-char *publisher[] = {"ACT", "Alpine", "Bombora", "Eksmo"};
+#define arrlen(arr) sizeof(arr) / sizeof(*arr)
+
+static char *authors[] = {"Glukhovskiy", "Asimov",   "King",    "Dostoevskiy", "Pushkin",
+                          "Savelyev",    "Perelman", "Arbuzov", "Hamilton"};
+static char *titles1[] = {"Cool", "Awesome", "Bad", "Dummy", "Moderate"};
+static char *titles2[] = {"Animals", " Nature",  " Friends", " Vibes", " Cars", " Automoblies",
+                          " People", " Student", " Apples",  " Cats",  " Dogs"};
+static char *titles3[] = {" of Kamchatka", " in Nigeria", " in Africa",    " of USA",
+                          " in USA",       " of China",   " in University"};
+static char *industry[] = {"Physics", "Math", "Programming", "Law", "Chemical"};
+static char *publisher[] = {"ACT", "Alpine", "Bombora", "Eksmo"};
 
 static size_t generate_in_range(size_t min, size_t max)
 {
@@ -19,17 +23,17 @@ void generate_entries(Table *table, size_t size)
     Book book;
     for (size_t i = 0; i < size; ++i)
     {
-        strcpy(book.authorSurname, authors[rand() % 8]);
-        strcpy(book.bookTitle, titles1[rand() % 5]);
-        strcat(book.bookTitle, titles2[rand() % 11]);
-        strcat(book.bookTitle, titles3[rand() % 7]);
-        strcpy(book.publisher, publisher[rand() % 4]);
+        strcpy(book.authorSurname, authors[rand() % arrlen(authors)]);
+        strcpy(book.bookTitle, titles1[rand() % arrlen(titles1)]);
+        strcat(book.bookTitle, titles2[rand() % arrlen(titles2)]);
+        strcat(book.bookTitle, titles3[rand() % arrlen(titles3)]);
+        strcpy(book.publisher, publisher[rand() % arrlen(publisher)]);
         book.pageCount = generate_in_range(100, 500);
         book.genre = generate_in_range(0, 2);
         switch (book.genre)
         {
         case TECHNICAL:
-            strcpy(book.data.technical.industry, industry[rand() % 5]);
+            strcpy(book.data.technical.industry, industry[rand() % arrlen(industry)]);
             book.data.technical.domestic = generate_in_range(0, 1);
             book.data.technical.publishYear = generate_in_range(1950, 2017);
             break;
